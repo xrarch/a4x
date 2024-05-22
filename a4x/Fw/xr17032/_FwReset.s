3e10251692707c1ca8362b8f40d97e93e96eba11
hyenasky <ashnwill49@gmail.com>
2024-05-22 05:43:27 -0600

    Build: move completely to new toolchain

--- a4x/Fw/xr17032/_FwReset.s
+++ a4x/Fw/xr17032/_FwReset.s
@@ -1,6 +1,6 @@
-;
-; Contains low level reset code for the ROM.
-;
+// 
+//  Contains low level reset code for the ROM.
+// 
 
 .section text
 
@@ -8,112 +8,112 @@
 .extern FwProcessorStartup
 .extern FwReset
 
-;vector 0 - nothing
+// vector 0 - nothing
     j    FwxExceptionVector
 
 .align 256
-;vector 1 - interrupt
+// vector 1 - interrupt
     j    FwxExceptionVector
 
 .align 256
-;vector 2 - syscall
+// vector 2 - syscall
     j    FwxExceptionVector
 
 .align 256
-;vector 3 - nothing
+// vector 3 - nothing
     j    FwxExceptionVector
 
 .align 256
-;vector 4 - bus error
+// vector 4 - bus error
     j    FwxExceptionVector
 
 .align 256
-;vector 5 - NMI
+// vector 5 - NMI
     j    FwxExceptionVector
 
 .align 256
-;vector 6 - breakpoint
+// vector 6 - breakpoint
     j    FwxExceptionVector
 
 .align 256
-;vector 7 - illegal instruction
+// vector 7 - illegal instruction
     j    FwxExceptionVector
 
 .align 256
-;vector 8 - privilege violation
+// vector 8 - privilege violation
     j    FwxExceptionVector
 
 .align 256
-;vector 9 - unaligned address
+// vector 9 - unaligned address
     j    FwxExceptionVector
 
 .align 256
-;vector 10 - nothing
+// vector 10 - nothing
     j    FwxExceptionVector
 
 .align 256
-;vector 11 - nothing
+// vector 11 - nothing
     j    FwxExceptionVector
 
 .align 256
-;vector 12 - page fault read
+// vector 12 - page fault read
     j    FwxExceptionVector
 
 .align 256
-;vector 13 - page fault write
+// vector 13 - page fault write
     j    FwxExceptionVector
 
 .align 256
-;vector 14 - nothing
+// vector 14 - nothing
     j    FwxExceptionVector
 
 .align 256
-;vector 15 - nothing
+// vector 15 - nothing
     j    FwxExceptionVector
 
 .align 256
 
-; We should now be at 0xFFFE1000, the reset vector.
+//  We should now be at 0xFFFE1000, the reset vector.
 
 FwxReset:
 .global FwxReset
-    ; Zero out RS.
+    //  Zero out RS.
 
     mtcr rs, zero
 
-    ; Invalidate the caches.
+    //  Invalidate the caches.
 
     li   t0, 3
     mtcr icachectrl, t0
     mtcr dcachectrl, t0
 
-    ; Set our exception block.
+    //  Set our exception block.
 
     lui  t0, zero, 0xFFFE0000
     mtcr eb, t0
 
-    ; If we aren't processor zero, go to the MP corrall and wait for an IPI.
+    //  If we aren't processor zero, go to the MP corrall and wait for an IPI.
 
     mfcr t0, whami
     bne  t0, .mp_corrall
 
-    ; Reset EBUS to quiescent state.
+    //  Reset EBUS to quiescent state.
 
     la   t0, 0xAABBCCDD
     mov  long [0xF8800000], t0, tmp=t1
 
-    ; Set the initial stack pointer to 1024 bytes.
+    //  Set the initial stack pointer to 1024 bytes.
 
     li   sp, 0x400
 
     j    FwReset
 
 .mp_corrall:
-    ; Set the stack pointer to 0x400 + (384 * id).
+    //  Set the stack pointer to 0x400 + (384 * id).
 
     li   sp, 0x400
-    add  sp, sp, t0 LSH 8 ; + 256 * id
-    add  sp, sp, t0 LSH 7 ; + 128 * id
+    add  sp, sp, t0 LSH 8 //  + 256 * id
+    add  sp, sp, t0 LSH 7 //  + 128 * id
 
     mov  a0, t0
     j    FwProcessorStartup
